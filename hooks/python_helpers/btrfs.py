9bdca4cf311bf61fcbb9dc4f7ea39e82b8dc236e
Wolf Gantert <parias@dragonriders.de>
2018-02-10 11:13:32 +0100

    remove code duplication in BtrfsCtrl::rollback
    BtrfsCtrl::rollback did creating and deleting a snapshot itself, change
    this to call the respective functions. This needed a change to snapshot
    creation, making it able to create r and rw snapshots (defaulting to r)

--- hooks/python_helpers/btrfs.py
+++ hooks/python_helpers/btrfs.py
@@ -37,19 +37,21 @@ class BtrfsCtrl:
         self.mntpoint.umount()
         os.rmdir(self.tmpmnt)
 
-    def snapshot(self, source, snapname):
+    def snapshot(self, source, snapname, read_only = True):
         if self.tmpmnt is None:
             raise Error('btrfs control mount is not established')
-        btrfs_cmd = 'btrfs subvolume snapshot -r %s/%s %s/%s' % (self.tmpmnt, source, self.tmpmnt, snapname)
+        if read_only == True:
+            r_str = '-r'
+        else:
+            r_str = ''
+        btrfs_cmd = 'btrfs subvolume snapshot %s %s/%s %s/%s' % (r_str, self.tmpmnt, source, self.tmpmnt, snapname)
         os.system(btrfs_cmd)
 
     def rollback(self, source, target):
         if self.tmpmnt is None:
             raise Error('btrfs control mount is not established')
-        btrfs_cmd = 'btrfs subvolume delete %s/%s' % (self.tmpmnt, target)
-        os.system(btrfs_cmd)
-        btrfs_cmd = 'btrfs subvolume snapshot %s/%s %s/%s' % (self.tmpmnt, source, self.tmpmnt, target)
-        os.system(btrfs_cmd)
+        self.rm_snapshot(target)
+        self.snapshot(source, target, False)
         mnt_handle = MntWrapper(self.source, self.target)
         mnt_handle.umount()
         mnt_handle.mount('subvol=portage')
