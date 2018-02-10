3f4f8d0daaedccaf4f22580181a2546467913d30
Wolf Gantert <parias@dragonriders.de>
2018-02-10 10:13:42 +0100

    gemato_verify.py: Add to tree
    This is a very simple hook which just checks the authenticity of the
    portage tree using app-portage/gemato and keys from
    app-crypt/gentoo-keys.

---
+++ hooks/sync_all_post/gemato-verify.py
@@ -0,0 +1,14 @@
+def hook_run_sync_all_post(env, hook_env):
+    import os
+    import shutil
+
+    ret = 0
+    key_location = "/var/lib/gentoo/gkeys/keyrings/gentoo/release/pubring.gpg"
+    portage_tree = "/usr/portage"
+    gemato_cmd = "gemato verify %s -K %s" % (portage_tree, key_location)
+    targets = hook_env["TARGETS"].split(" ")
+    for name in targets:
+        if name == "gentoo":
+            print "Verifying integrity of %s with keys from %s" % (portage_tree, key_location)
+            ret = os.system(gemato_cmd)
+    return ret
