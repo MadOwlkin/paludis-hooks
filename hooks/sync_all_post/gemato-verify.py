072b156e41b1c048318ce53eb2277e4697f1501e
Wolf Gantert <parias@dragonriders.de>
2018-02-10 12:53:53 +0100

    gemato-verify: force signed top manifest
    Without providing -s to gemato a signature for the toplevel manifest is
    optional. Enforce a signed Manifest by default.

--- hooks/sync_all_post/gemato-verify.py
+++ hooks/sync_all_post/gemato-verify.py
@@ -5,7 +5,7 @@ def hook_run_sync_all_post(env, hook_env):
     ret = 0
     key_location = "/var/lib/gentoo/gkeys/keyrings/gentoo/release/pubring.gpg"
     portage_tree = "/usr/portage"
-    gemato_cmd = "gemato verify %s -K %s" % (portage_tree, key_location)
+    gemato_cmd = "gemato verify %s -K %s -s" % (portage_tree, key_location)
     targets = hook_env["TARGETS"].split(" ")
     for name in targets:
         if name == "gentoo":
